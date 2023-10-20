27bd9b339f5f99928bc4623f0b61877e21fd8815
jdwalker-swe <jdwalker.swe@gmail.com>
2023-10-20 14:10:55 -0400

    upgrade scipy==1.10.0

--- pyitlib/pyitlib_version.py
+++ pyitlib/pyitlib_version.py
@@ -1,4 +1,4 @@
 """pyitlib version"""
 
-__version__ = u'0.2.4'
+__version__ = u'0.2.5'
 # __version_short__ = u'0.2'
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     packages=['pyitlib', ],
     zip_safe=False,
     install_requires=[
-        'pandas==1.4.1',
+        'pandas==1.5.3',
         'numpy==1.22.3',
         'scikit-learn==1.0.2',
         'scipy==1.10.0',
